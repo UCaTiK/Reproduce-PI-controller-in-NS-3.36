cc152a2c9768c24ef8c85ee1f1b24ca14f1226ec
UCaTiK <49482726+UCaTiK@users.noreply.github.com>
2023-06-05 12:25:00 +0300

    Update pi-queue-disc.cc

--- model/pi-queue-disc.cc
+++ model/pi-queue-disc.cc
@@ -1,30 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * Copyright (c) 2016 NITK Surathkal
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Authors: Priya S Tavarmani <priyast663@gmail.com>
- *          Viyom Mittal <viyommittal@gmail.com>
- *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
- */
-
-/*
- * PORT NOTE: This code was ported from ns-2.36rc1 (queue/pi.cc).
- * Most of the comments are also ported from the same.
- */
-
 #include "ns3/log.h"
 #include "ns3/enum.h"
 #include "ns3/uinteger.h"
